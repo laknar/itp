bf8f7268a7ee33b63dc29b6bd805aebb1abe03bf
kasunajax <35171019+kasunajax@users.noreply.github.com>
2018-08-03 23:55:21 +0530

    Update Test.java

--- ITP/src/Test.java
+++ ITP/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main() {
 		System.out.println("lula");	
 		System.out.println("Hello world");
+		System.out.println("[Lakna The Great]");
 		System.out.println("New Change");
 		System.out.println("Gon Deshan");
 	}
